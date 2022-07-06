256e5af003362ae507fd85b9b516ee59609ef401
aguy tech <aguytech@free.fr>
2022-07-06 11:35:10 +0200

    jammy:install add halt between parts

--- install.sh
+++ install.sh
@@ -29,6 +29,8 @@ for _PART in ${_PARTS_MAN}; do
 	if ! _parthave ${_PART} ${_FILE_DONE}; then
 		grep -q "^# ${_PART}" ${_FILE_CONF} || echo "# ${_PART}" >> ${_FILE_CONF}
 		_source "${_PATH_BASE}/sub/${_PART}"
+		_echoA "Valid to continue"
+		_askno
 	fi
 done
 
