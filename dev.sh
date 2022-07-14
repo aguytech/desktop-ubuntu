911b3e8a4d8f2bc559232006368472347d3b32c5
aguy tech <aguytech@free.fr>
2022-07-14 08:52:04 +0200

    jammy: move inc from sub/ to bs module

--- dev.sh
+++ dev.sh
@@ -5,13 +5,15 @@
 ######################## CONF
 _TRACE=debug
 _PATH_BASE=$( readlink -f ${0%/*} )
+_PATH_CONF=${HOME}/.config/desktop-install
+_PATH_LOG=/var/log/desktop-install
 _CMD="sudo apt"
 _CMD_INS="sudo apt install -y"
 
 _FILE_CONF=${HOME}/.config/desktop-install/dev.conf
 _FILE_DONE=${HOME}/.config/desktop-install/dev.done
 
-file=${_PATH_BASE}/sub/inc
+file=${_PATH_BASE}/bs/inc
 ! [ -f ${file} ] && echo "Unable to find file: ${file}" && exit 1
 ! . ${file} && echo "Errors while importing ${file}" && exit 1
 
@@ -41,10 +43,8 @@ done
 _PARTS_ALL=$( ls ${_PATH_BASE}/dev -I ${_PARTS_MAN// / -I } )
 
 while [ "${_PART}" != "quit" ]; do
-
 	_SDATE=$(date +%s) # renew _SDATE
 	parts_made=" $( cat "${_FILE_DONE}" | xargs ) "
-	
 	parts2do=" "
 	for part in ${_PARTS_ALL}; do
 		[ "${parts_made/ ${part} }" = "${parts_made}" ] && parts2do+="$part "
